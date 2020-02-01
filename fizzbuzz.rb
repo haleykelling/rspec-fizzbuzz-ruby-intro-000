d674f6aa54d243ee3a9bf0ca7874ab86ef0e40bf
Haley Kelling <haley@kellings.com>
2020-02-01 17:48:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
+  binding.pry
   if number % 3 == 0
     puts "Fizz"
   elsif number % 5 == 0

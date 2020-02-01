999fc2919b457cfaf7987ec002580733de1c97b3
Haley Kelling <haley@kellings.com>
2020-02-01 17:52:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(number)
   require 'pry'; binding.pry
   if number % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif number % 15 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   end
 end

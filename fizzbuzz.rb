5a2590220833954fd7c97c8ce0b5404fb452220b
Haley Kelling <haley@kellings.com>
2020-02-01 17:21:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  number = gets.strip
+  if number / 3
+    puts "Fizz"
+  else number / 5
+    puts "Buzz"
+  elsif number / 15
+    puts "FizzBuzz"
+  end
